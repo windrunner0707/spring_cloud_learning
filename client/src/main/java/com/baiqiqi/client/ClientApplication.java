bb014ed75f86c25fb3fc80ae1e0495c813d89e3d
baiqiqi <baiqiqi@ascs.tech>
2018-06-01 15:22:00 +0800

    fix hystrix dashboard and init config server

--- client/src/main/java/com/baiqiqi/client/ClientApplication.java
+++ client/src/main/java/com/baiqiqi/client/ClientApplication.java
@@ -1,7 +1,9 @@
 package com.baiqiqi.client;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -25,6 +27,21 @@ public class ClientApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 使用SpringBoot2.0时如果不注入这个bean会在使用hystrix的后台时报错，因为SpringBoot的默认路径不是 "/hystrix.stream"
+     * 报错提示：Unable to connect to Command Metric Stream
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
